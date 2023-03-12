c14c2dc061fbd559b421105ce39d5ff89b6f4023
Sinforge <vlad.vlasov77@mail.ru>
2023-03-12 22:40:45 +0300

    add authentication for services

--- ArticleService/Program.cs
+++ ArticleService/Program.cs
@@ -1,18 +1,36 @@
 using System.Security.Claims;
+using System.Text;
 using ArticleService.Data;
 using ArticleService.DTOs;
 using ArticleService.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddAuthentication("Cookies")
-    .AddCookie();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        var jwtConfig = builder.Configuration.GetSection("Audience");
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtConfig["Secret"])),
+            ValidateIssuer = true,
+            ValidIssuer = jwtConfig["Iss"],
+            ValidateAudience = true,
+            ValidAudience = jwtConfig["Aud"],
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            RequireExpirationTime = true,
+
+        };
+    });
 builder.Services.AddAuthorization();
 builder.Services.AddDbContext<ApplicationContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString(name: "DefaultConnection")));
 builder.Services.AddScoped<IArticleRepo, ArticleRepo>();
-builder.Services.AddAuthorization();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 var app = builder.Build();
--- OcelotGateway/Program.cs
+++ OcelotGateway/Program.cs
@@ -1,8 +1,29 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        var jwtConfig = builder.Configuration.GetSection("Audience");
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtConfig["Secret"])),
+            ValidateIssuer = true,
+            ValidIssuer = jwtConfig["Iss"],
+            ValidateAudience = true,
+            ValidAudience = jwtConfig["Aud"],
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            RequireExpirationTime = true,
 
+        };
+    });
+builder.Services.AddAuthorization();
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
