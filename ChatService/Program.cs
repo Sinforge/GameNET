0bfb9d5f6e4f5d0634efc278550b48c2e880bd4a
Vlad <vlad.vlasov77@mail.ru>
2023-03-28 22:15:15 +0300

    fix docker compose

--- ChatService/Program.cs
+++ ChatService/Program.cs
@@ -6,14 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("badWords.json");
 
 
-builder.Services.AddSignalR(options => options.AddFilter<BadWordsHubFilter>());      // подключема сервисы SignalR
+builder.Services.AddSignalR(options => options.AddFilter<BadWordsHubFilter>());      // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ SignalR
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.MapHub<ChatHub>("/chat");   // ChatHub будет обрабатывать запросы по пути /chat
+app.MapHub<ChatHub>("/chat");   // ChatHub пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅ /chat
 
 app.Run();
 
